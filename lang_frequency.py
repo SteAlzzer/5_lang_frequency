09c73a0581861b699ca34fe083faf85b9e87e989
SteAlzzerMBAW <mk@cnpo.ru>
2016-11-29 00:22:25 +0400

    Добавлена поддержка только русских слов

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,24 +3,30 @@ import re
 from collections import Counter
 from optparse import OptionParser
 
-import time
+import time #delete
 
-def load_data(filepath):
+def load_data(filepath, reg_exp_rus=False):
     '''
     Функция загрузки данных из файла
     Аргументы: filepath - путь к файлу
+               reg_exp_rus - какой тип регулярного выражения использовать.
+                    0 - Все найденные слова
+                    1 - Только русские слова
     Возвращает список всех найденных слов в файле
     '''
     if not os.path.isfile(filepath):
         return None 
     data = []
+    
+    if reg_exp_rus:
+        reg_exp = r'[А-Яа-я]+'
+    else:
+        reg_exp = r'\w+'
+
     for line in open(filepath, encoding='utf-8'):
-        data.extend(re.findall(r'\w+', line.lower().rstrip('\r\n')))
+        data.extend(re.findall(reg_exp, line.lower().rstrip('\r\n')))
+        # data.extend(re.findall(r'\w+', line.lower().rstrip('\r\n')))
     return data
-    # with open(filepath, encoding='utf-8') as file_handler:
-    #     text = file_handler.read()
-    #     data = re.findall(r'\w+', text.lower()) 
-    #     return data
 
 
 def get_most_frequent_words(text, amount_to_show=10):
@@ -33,10 +39,11 @@ def get_most_frequent_words(text, amount_to_show=10):
 
 
 if __name__ == '__main__':
-    usage = 'Usage: %prog -p path_to_text_file [-a amount_to_show]'
+    usage = 'Usage: %prog -p path_to_text_file [-r] [-a amount_to_show]'
     parser = OptionParser(usage=usage)
 
     parser.add_option('-p', '--path', action='store', type='string', help='Путь до текстового файла')
+    parser.add_option('-r', '--rus', action='store_true', default=False, help='Искать только русские слова')
     parser.add_option('-a', '--amount_to_show', action='store', type='int', help='Количество отображаемых слов в топе')
     
     options, arguments = parser.parse_args()
@@ -51,13 +58,18 @@ if __name__ == '__main__':
         amount = options.amount_to_show
     else:
         amount = 10
+
     start_time = time.time()
-    words_in_text = load_data(path)
+
+    words_in_text = load_data(path, options.rust)
     print(u'Файл прочитан')
+
     print('{}'.format(time.time() - start_time))
+
     if words_in_text is None:
         print('Джонни, у нас проблема с файлом. Так дело не пойдёт')
         exit(-1)
 
-    get_most_frequent_words(words_in_text, amount)    
+    get_most_frequent_words(words_in_text, amount) 
+
     print('{}'.format(time.time() - start_time))
