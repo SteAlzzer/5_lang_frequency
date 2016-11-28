f8b4afc7e29a583382990ff5aac49e86bef9d9d1
SteAlzzerMBAW <mk@cnpo.ru>
2016-11-28 09:02:07 +0400

    Тестируем поддержку больших файлов

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,6 +3,8 @@ import re
 from collections import Counter
 from optparse import OptionParser
 
+import time
+
 def load_data(filepath):
     '''
     Функция загрузки данных из файла
@@ -11,10 +13,14 @@ def load_data(filepath):
     '''
     if not os.path.isfile(filepath):
         return None 
-    with open(filepath, encoding='utf-8') as file_handler:
-        text = file_handler.read()
-        data = re.findall(r'\w+', text.lower()) 
-        return data
+    data = []
+    for line in open(filepath, encoding='utf-8'):
+        data.extend(re.findall(r'\w+', line.lower().rstrip('\r\n')))
+    return data
+    # with open(filepath, encoding='utf-8') as file_handler:
+    #     text = file_handler.read()
+    #     data = re.findall(r'\w+', text.lower()) 
+    #     return data
 
 
 def get_most_frequent_words(text, amount_to_show=10):
@@ -23,7 +29,7 @@ def get_most_frequent_words(text, amount_to_show=10):
     string_to_show = '{:0' + digits_size + '}' + '| {}\t| {}'
     for num, word in enumerate(words_counter.most_common(amount_to_show)):
         print(string_to_show.format(num+1, word[0], word[1]))
-
+    del words_counter
 
 
 if __name__ == '__main__':
@@ -45,11 +51,13 @@ if __name__ == '__main__':
         amount = option.amount_to_show
     else:
         amount = 10
-
+    start_time = time.time()
     words_in_text = load_data(path)
-
+    print(u'Файл прочитан')
+    print('{}'.format(time.time() - start_time))
     if words_in_text is None:
         print('Джонни, у нас проблема с файлом. Так дело не пойдёт')
         exit(-1)
 
     get_most_frequent_words(words_in_text, amount)    
+    print('{}'.format(time.time() - start_time))
